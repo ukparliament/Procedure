5ef940867e6383d2083aad56ce79c41593c6cfef
Raphael Leung <lcyraphael@gmail.com>
2018-04-27 20:15:40 +0100

    Implemented GraphML format for procedures, verified with yEd

--- Procedure.Web/Controllers/ProceduresController.cs
+++ Procedure.Web/Controllers/ProceduresController.cs
@@ -14,6 +14,10 @@ using VDS.RDF;
 using System.Xml.Linq;
 using System.Linq;
 using Parliament.Rdf.Serialization;
+using VDS.RDF.Query;
+using VDS.RDF.Parsing;
+using System.IO;
+using System.Xml;
 
 namespace Procedure.Web.Controllers
 {
@@ -43,6 +47,7 @@ namespace Procedure.Web.Controllers
             return View(viewModel);
         }
 
+        // Renders DOT/Graphviz, an open graph description language (https://www.graphviz.org/doc/info/lang.html)
         [Route("{id:int}/graph")]
         public ActionResult Graph(int id)
         {
@@ -58,6 +63,7 @@ namespace Procedure.Web.Controllers
             return File(output, "image/svg+xml");
         }
 
+        // Return graph in DOT string
         [Route("{id:int}/graph.dot")]
         public ActionResult GraphDot(int id)
         {
@@ -108,7 +114,8 @@ namespace Procedure.Web.Controllers
             return builder.ToString();
         }
 
-        [Route("{id:int}/graphml")]
+        // Return graph in GraphML, an XML-based graph format (http://graphml.graphdrawing.org/primer/graphml-primer.html) 
+        [Route("{id:int}/graph.graphml")]
         public ActionResult GraphML(int id)
         {
             string routeResponse = null;
@@ -119,33 +126,60 @@ namespace Procedure.Web.Controllers
             JObject jsonRoute = (JObject)JsonConvert.DeserializeObject(routeResponse);
             List<RouteItem> routes = ((JArray)jsonRoute.SelectToken("value")).ToObject<List<RouteItem>>();
 
+            // Using Parliament nuget packages, map user-defined classes into ontology-aligned interfaces which all implement iResource interface
+            // This allows us to work with IGraph objects
             IEnumerable<IProcedureRoute> IRoutes = routes.Select(r => r.GiveMeMappedObject());
 
             RdfSerializer serializer = new RdfSerializer();
             IGraph graph = serializer.Serialize(IRoutes, typeof(IProcedureRoute).Assembly.GetTypes());
 
+            SparqlQueryParser parser = new SparqlQueryParser();
             // Nodes 
-            IEnumerable<Triple> stepTriples = graph.GetTriplesWithObject(new System.Uri("https://id.parliament.uk/schema/ProcedureStep"));
+            SparqlQuery q1 = parser.ParseFromString("PREFIX : <https://id.parliament.uk/schema/> SELECT ?step ?stepName WHERE {?step a :ProcedureStep; :procedureStepName ?stepName. }");
+            SparqlResultSet nodes = (SparqlResultSet)graph.ExecuteQuery(q1);
+            
             // Edges
-            IEnumerable<Triple> routeTriples = graph.GetTriplesWithObject(new System.Uri("https://id.parliament.uk/schema/ProcedureRoute"));
+            SparqlQuery q2 = parser.ParseFromString("PREFIX : <https://id.parliament.uk/schema/> SELECT ?route ?fromStep ?toStep WHERE {?route a :ProcedureRoute; :procedureRouteIsFromProcedureStep ?fromStep; :procedureRouteIsToProcedureStep ?toStep.}");
+            SparqlResultSet edges = (SparqlResultSet)graph.ExecuteQuery(q2);
 
-            // http://graphml.graphdrawing.org/primer/graphml-primer.html
+            // Create GraphML 
+            StringWriter sw = new Utf8StringWriter();
+            XmlWriterSettings xws = new XmlWriterSettings();
+            xws.OmitXmlDeclaration = false; 
+            xws.Indent = true;
 
-            XNamespace ns = "http://graphml.graphdrawing.org/xmlns";
+            using (XmlWriter xw = XmlWriter.Create(sw, xws))
+            {
+                XNamespace ns = "http://graphml.graphdrawing.org/xmlns";
+                XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+                XNamespace xsiSchemaLocation = "http://graphml.graphdrawing.org/xmlns http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd";
+                // For yEd, not essential to GraphML. But good for verification
+                XNamespace y = "http://www.yworks.com/xml/graphml";
 
-            XDocument doc = new XDocument(
-                new XDeclaration("1.0", "UTF-8", "yes"),
-                new XElement(ns + "graphml",
-                    new XElement("graph",
-                        new XAttribute("id", "G"), new XAttribute("edgedefault", "directed"),
-                        stepTriples.Select(t => new XElement("node", new XAttribute("id", t.Subject.GetHashCode()), new XElement("data", new XText(t.Subject.ToString()))))
-                        // graph.Nodes.Select(n => new XElement("node", new XAttribute("id", n.GetHashCode())))
-                    )));
+                XDocument doc = new XDocument(
+                    new XDeclaration("1.0", "UTF-8", "yes"),
+                    new XElement(ns + "graphml",
+                        new XAttribute(XNamespace.Xmlns + "xsi", xsi),
+                        new XAttribute(xsi + "schemaLocation", xsiSchemaLocation),
+                        new XAttribute(XNamespace.Xmlns + "y", y),
+                        new XElement(ns + "key", new XAttribute("for", "node"), new XAttribute("id", "d0"), new XAttribute("yfiles.type", "nodegraphics")),
+                        new XElement(ns + "graph", new XAttribute("id", "G"), new XAttribute("edgedefault", "directed"),
+                            nodes.Select(n => new XElement(ns + "node", new XAttribute("id", n["step"]), new XElement(ns + "data", new XAttribute("name", n["stepName"]), new XAttribute("key", "d0"), new XElement(y + "ShapeNode", new XElement(y + "NodeLabel", new XAttribute("visible", "true"), new XAttribute("autoSizePolicy", "content"), new XText(n["stepName"].ToString())))))),
+                            edges.Select(e => new XElement(ns + "edge", new XAttribute("id", e["route"]), new XAttribute("source", e["fromStep"]), new XAttribute("target", e["toStep"])))
+                        )));
+                doc.WriteTo(xw);
+            }
 
-            // HttpResponseMessage Response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            return Content(sw.ToString(), "application/xml");
 
-            return Content(doc.ToString(), "application/xml");
+            }
 
+        // Using LINQ to XML, if you choose to write to stream instead of disk, the XML doc will automatically be encoded to match 
+        // the stream's default encoding, in this case utf-16, which is not what we want
+        // See https://blogs.msdn.microsoft.com/ericwhite/2010/03/08/serializing-encoded-xml-documents-using-linq-to-xml/
+        private class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding { get { return Encoding.UTF8; } }
         }
 
 
