690596171f19534965f48d0ddf75609aa166bbbb
Raphael Leung <lcyraphael@gmail.com>
2018-05-02 16:46:18 +0100

    Reflect change in Sharepoint list column name

--- Procedure.Web/Models/BusinessItem.cs
+++ Procedure.Web/Models/BusinessItem.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Procedure.Web.Models
 {
     public class BusinessItem : BaseSharepointItem
     {
         public List<SharepointLookupItem> BelongsTo { get; set; }
+
+        [JsonProperty(PropertyName = "ActualisesProcedureStep_x003a_Tr")]
         public List<SharepointLookupItem> Actualises { get; set; }
     }
 }
