2698da0c36205dbd2ee99ca84e9708b85690b56a
Raphael Leung <lcyraphael@gmail.com>
2018-05-02 17:44:38 +0100

    Commented prescient code changes for work packages

--- Procedure.Web/Models/WorkPackageItem.cs
+++ Procedure.Web/Models/WorkPackageItem.cs
@@ -1,7 +1,21 @@
 ﻿namespace Procedure.Web.Models
 {
+    // * Commented code for when work package items no longer have Titles and cannot inherit from BaseSharepointItem class
+    
+        // public class WorkpackageItem
     public class WorkPackageItem : BaseSharepointItem
     {
         public SharepointLookupItem SubjectTo { get; set; }
+
+        // public string TripleStoreId { get; set;} 
+        // public SharepointLookupItem WorkPackageableThing { get; set;}
+
+        //public IWorkPackage GiveMeMappedObject()
+        //{
+        //    IWorkPackage result = new Parliament.Model.WorkPackage();
+        //    result.Id = new System.Uri($"https://id.parliament.uk/{TripleStoreId}");
+           
+        //    return result;
+        //}
     }
 }
