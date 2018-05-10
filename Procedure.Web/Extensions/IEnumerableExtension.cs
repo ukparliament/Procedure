13c04c48fd64803457c79b6b6f3572ce3b3a6646
Raphael Leung <lcyraphael@gmail.com>
2018-05-10 11:15:51 +0100

    Add button for graphviz

---
+++ Procedure.Web/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Procedure.Web.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static bool IsNullOrEmpty<T> (this IEnumerable<T> ienumerable)
+        {
+            return (ienumerable == null) || (!ienumerable.Any());
+        }
+    }
+}
